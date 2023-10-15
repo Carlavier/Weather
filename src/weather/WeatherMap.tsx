67dafdb4401f8ff43a7f681437012f3a6efc07f0
Nguyễn Lê Bảo Nam <namto10042004@gmail.com>
2023-10-15 23:00:58 +0700

    Commit

--- src/weather/WeatherMap.tsx
+++ src/weather/WeatherMap.tsx
@@ -1,9 +1,16 @@
 import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
 
 import { MapContainer, Marker, TileLayer } from 'react-leaflet'
 import { coordinate } from "./Home";
 
 function WeatherMap({ coord: {lat, lon} }: Props) {
+    const mapIcon = L.icon({
+        iconUrl: 'src\\assets\\map_icon.png',
+        iconSize: new L.Point(41, 41),
+        iconAnchor: [20.5, 20.5 + 22]
+    })
+
     return(
         <div style={{}}>
             <MapContainer center={[lat, lon]} zoom={13} scrollWheelZoom={false}>
@@ -11,7 +18,7 @@ function WeatherMap({ coord: {lat, lon} }: Props) {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[lat, lon]} />
+                <Marker position={[lat, lon]} icon={mapIcon}/>
             </MapContainer>
         </div>
     );
